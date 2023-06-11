32246fc065cdcf568117f3f4d3a81d381d71c6a4
Hiroyuki Kaneko <hkaneko@redhat.com>
2023-06-11 09:11:54 +0900

    verified v4.12

--- scripts/install_clis.sh
+++ scripts/install_clis.sh
@@ -4,6 +4,7 @@ echo "Knative CLI install"
 wget "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/serverless/latest/kn-linux-amd64.tar.gz"
 
 tar -xzvf kn-linux-amd64.tar.gz -C /usr/bin/
+mv /usr/bin/kn-linux-amd64 /usr/bin/kn
 
 echo "Tekton CLI install"
 wget "https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/pipeline/latest/tkn-linux-amd64.tar.gz"
